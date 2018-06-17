6532c4d4d8dd6c20bee93450f4211bb0d631cd7f
ZhangRui111 <zhangruisg111@163.com>
2018-06-17 10:34:55 +0800

    rebuild chapter_6 project

--- chapter_6_the_cliff.py
+++ chapter_6_the_cliff.py
@@ -6,7 +6,13 @@ EPSILON = 0.4
 EPSILON_DECAY_LINE = 100000
 GAMMA = 0.9
 MAX_EPISODES = 500000
+
 TEST_EPISODES = 20
+RECORD_EPISODES_SMALL = 10
+RECORD_EPISODES_LARGE = 1000
+LOGS_PATH_S = './logs/chapter_6/sarsa/'
+LOGS_PATH_ES = './logs/chapter_6/expected_sarsa/'
+LOGS_PATH_QL = './logs/chapter_6/q_learning/'
 
 
 class Cliff(object):
@@ -39,17 +45,17 @@ class Cliff(object):
         self.terminal = False
 
     def reset_cliff(self):
-        self.reward = 0
-        self.terminal = False
         """Reset the cliff, put the agent at start point.
         """
+        self.reward = 0
+        self.terminal = False
         self.current_p = [4, 1]
         return self.current_p
 
     def random_reset_cliff(self):
         self.reward = 0
         self.terminal = False
-        """Reset the cliff, put the agent at random point
+        """Reset the cliff, put the agent at random point, with probability 0.4 at start point [4, 1]
         """
         r = random.uniform(0, 1)
         if r < 0.4:
@@ -65,11 +71,6 @@ class Cliff(object):
 
     def move(self, action):
         """move the agent following action.
-
-        :param action:
-        :return:
-            self.terminal
-            self.reward
         """
         old_p = [self.current_p[0], self.current_p[1]]
         if action == 0:
@@ -80,22 +81,24 @@ class Cliff(object):
             self.update_current_p(1, -1)
         if action == 3:
             self.update_current_p(1, 1)
-
+        # case Edge:
         if self.current_p[0] % 5 == 0 or self.current_p[1] % 7 == 0:
             self.current_p = old_p
             self.terminal = False
             self.reward = -1
         else:
+            # case Goal Point:
             if self.current_p == [4, 6]:
                 self.terminal = True
                 self.reward = 100
+            # case Cliff:
             elif self.current_p[0] == 4 and self.current_p[1] != 1:
                 self.terminal = True
                 self.reward = -100
+            # case Safe Road:
             else:
                 self.terminal = False
                 self.reward = -1
-
         return old_p, self.terminal, self.reward, self.current_p
 
 
@@ -108,11 +111,17 @@ class Brain(object):
     def map_p_to_row(self, current_p):
         """A map from current_p to row(state index) in Q_table.
         """
-        row = (current_p[0]-1)*6+(current_p[1]-1)
+        row = (current_p[0] - 1) * 6 + (current_p[1] - 1)
         return row
 
     def epsilon_greedy_action(self, row, episode, if_set_epsilon, epsilon_value):
         """Epsilon greedy policy for selecting action.
+
+        :param row: row index in Q_table used to greedy policy.
+        :param episode: episode number used to epsilon decay.
+        :param if_set_epsilon: whether to set epsilon by hand (when testing).
+        :param epsilon_value: epsilon value set by hand.
+        :return: selected action
         """
         if if_set_epsilon:
             epsilon = epsilon_value
@@ -125,13 +134,12 @@ class Brain(object):
         if r < epsilon:
             action = np.random.random_integers(0, 3)
         else:
-            # action = np.argmax(self.Q_table[row, :])
-            # When there are more than one max q, we choose one from them randomly.
             state_values = self.Q_table[row, :]
             num = list(state_values).count(state_values.max())
             if num == 1:
                 action = np.argmax(state_values)
             else:
+                # When there are more than one max q, we choose one from them randomly.
                 max_action_index = np.where(state_values == state_values.max())
                 list_q = list(max_action_index[0])
                 action = random.choice(list_q)
@@ -139,6 +147,8 @@ class Brain(object):
 
 
 def sarsa(if_random_init):
+    """Sarsa algorithm.
+    """
     scores = []
     the_brain = Brain()
     the_cliff = Cliff()
@@ -159,23 +169,25 @@ def sarsa(if_random_init):
             the_brain.Q_table[row, a] = the_brain.Q_table[row, a] + ALPHA * TD_error
             s = s_  # I have done this in Cliff.move(), so this line can be removed.
             a = a_
-        if i <= 10000 and i % 10 == 0:
+        if i <= 10000 and i % RECORD_EPISODES_SMALL == 0:
             scores.append(test_sarsa(the_cliff, the_brain))
             if i == 10000:
-                with open('./logs/sarsa/scores_s_10000.txt', 'w') as file:
+                with open(LOGS_PATH_S+'scores_s_10000.txt', 'w') as file:
                     file.write(str(scores))
-        if i > 10000 and i % 1000 == 0:
+        if i > 10000 and i % RECORD_EPISODES_LARGE == 0:
             print('Episode {0} end with reward:{1}'.format(i, r))
             scores.append(test_sarsa(the_cliff, the_brain))
         if i % 100000 == 0:
-            with open('./logs/sarsa/'+str(i)+'_Q_table_s.txt', 'w') as file:
+            with open(LOGS_PATH_S+str(i)+'_Q_table_s.txt', 'w') as file:
                 file.write(str(the_brain.Q_table))
-    with open('./logs/sarsa/scores_s.txt', 'w') as file:
+    with open(LOGS_PATH_S+'scores_s.txt', 'w') as file:
         file.write(str(scores))
     print(the_brain.Q_table)
 
 
 def expected_sarsa(if_random_init):
+    """Expected sarsa algorithm.
+    """
     scores = []
     the_brain = Brain()
     the_cliff = Cliff()
@@ -197,23 +209,25 @@ def expected_sarsa(if_random_init):
             the_brain.Q_table[row, a] = the_brain.Q_table[row, a] + ALPHA * TD_error
             s = s_  # I have done this in Cliff.move(), so this line can be removed.
             a = a_
-        if i <= 10000 and i % 10 == 0:
+        if i <= 10000 and i % RECORD_EPISODES_SMALL == 0:
             scores.append(test_sarsa(the_cliff, the_brain))
             if i == 10000:
-                with open('./logs/expected_sarsa/scores_es_10000.txt', 'w') as file:
+                with open(LOGS_PATH_ES+'scores_es_10000.txt', 'w') as file:
                     file.write(str(scores))
-        if i > 10000 and i % 1000 == 0:
+        if i > 10000 and i % RECORD_EPISODES_LARGE == 0:
             print('Episode {0} end with reward:{1}'.format(i, r))
             scores.append(test_sarsa(the_cliff, the_brain))
         if i % 100000 == 0:
-            with open('./logs/expected_sarsa/'+str(i)+'_Q_table_es.txt', 'w') as file:
+            with open(LOGS_PATH_ES+str(i)+'_Q_table_es.txt', 'w') as file:
                 file.write(str(the_brain.Q_table))
-    with open('./logs/expected_sarsa/scores_es.txt', 'w') as file:
+    with open(LOGS_PATH_ES+'scores_es.txt', 'w') as file:
         file.write(str(scores))
     print(the_brain.Q_table)
 
 
 def test_sarsa(the_cliff, the_brain):
+    """Test module for sarsa algorithm and expected sarsa algorithm.
+    """
     score = []
     scores = []
     for i in range(TEST_EPISODES):
@@ -234,6 +248,8 @@ def test_sarsa(the_cliff, the_brain):
 
 
 def q_learning(if_random_init):
+    """Q-learning algorithm.
+    """
     scores = []
     the_brain = Brain()
     the_cliff = Cliff()
@@ -252,23 +268,25 @@ def q_learning(if_random_init):
             TD_error = r + GAMMA * the_brain.Q_table[row_, :].max() - the_brain.Q_table[row, a]
             the_brain.Q_table[row, a] = the_brain.Q_table[row, a] + ALPHA * TD_error
             s = s_  # I have done this in Cliff.move(), so this line can be removed.
-        if i <= 10000 and i % 10 == 0:
+        if i <= 10000 and i % RECORD_EPISODES_SMALL == 0:
             scores.append(test_q_learning(the_cliff, the_brain))
             if i == 10000:
-                with open('./logs/q_learning/scores_ql_10000.txt', 'w') as file:
+                with open(LOGS_PATH_QL+'scores_ql_10000.txt', 'w') as file:
                     file.write(str(scores))
-        if i > 10000 and i % 1000 == 0:
+        if i > 10000 and i % RECORD_EPISODES_LARGE == 0:
             print('Episode {0} end with reward:{1}'.format(i, r))
             scores.append(test_q_learning(the_cliff, the_brain))
         if i % 100000 == 0:
-            with open('./logs/q_learning/'+str(i)+'_Q_table_ql.txt', 'w') as file:
+            with open(LOGS_PATH_QL+str(i)+'_Q_table_ql.txt', 'w') as file:
                 file.write(str(the_brain.Q_table))
-    with open('./logs/q_learning/scores_ql.txt', 'w') as file:
+    with open(LOGS_PATH_QL+'scores_ql.txt', 'w') as file:
         file.write(str(scores))
     print(the_brain.Q_table)
 
 
 def test_q_learning(the_cliff, the_brain):
+    """Test module for Q-learning algorithm.
+    """
     score = []
     scores = []
     for i in range(TEST_EPISODES):
@@ -287,7 +305,7 @@ def test_q_learning(the_cliff, the_brain):
 
 
 def main():
-    # sarsa(True)
+    sarsa(True)
     q_learning(True)
     expected_sarsa(True)
 
